749c2fe0e09142ae1fb60895e916c3297c57357d
Gabriel Gonzalez <gabgcdev@gmail.com>
2023-12-04 20:44:12 -0500

    solution to day4 problem2

--- day4.js
+++ day4.js
@@ -4,22 +4,40 @@ function loadData() {
   return fs.readFileSync("day4-data.txt").toString("utf-8").split("\n");
 }
 
+function matchingNumbers(card) {
+  let points = 0;
+  const winningNumbers = card
+    .substring(card.indexOf(":") + 2, card.indexOf("|") - 1)
+    .match(/\d+/g);
+  const cardNumbers = card.substring(card.indexOf("|") + 2).match(/\d+/g);
+  for (let num of cardNumbers) {
+    if (winningNumbers.includes(num)) {
+      points += 1;
+    }
+  }
+  return points;
+}
+
 function main1() {
   return loadData()
     .map((card) => {
-      let points = 0;
-      const winningNumbers = card
-        .substring(card.indexOf(":") + 2, card.indexOf("|") - 1)
-        .match(/\d+/g);
-      const cardNumbers = card.substring(card.indexOf("|") + 2).match(/\d+/g);
-      for (let num of cardNumbers) {
-        if (winningNumbers.includes(num)) {
-          points += 1;
-        }
-      }
-      return points === 0 ? 0 : Math.pow(2, points - 1);
+      const n = matchingNumbers(card);
+      return n === 0 ? 0 : Math.pow(2, n - 1);
     })
     .reduce((prev, curr) => (prev += curr));
 }
 
+function main2() {
+  const data = loadData();
+  const cardInstances = data.map(() => 1);
+  for (let i = 0; i < data.length; i++) {
+    const card = data[i];
+    for (let j = 0; j < matchingNumbers(card); j++) {
+      cardInstances[i + j + 1] += cardInstances[i];
+    }
+  }
+  return cardInstances.reduce((prev, curr) => (prev += curr));
+}
+
 console.log("Solution to problem 1:", main1());
+console.log("Solution to problem 2:", main2());
