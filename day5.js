75a639e53a707a5014dad85c139f725cb86d3d2a
Gabriel Gonzalez <ggonzalez@builtglobal.com>
2023-12-05 20:29:18 -0500

    solution day5 p2. super inefficient lol

--- day5.js
+++ day5.js
@@ -46,40 +46,43 @@ function main2() {
     .split(" ")
     .map((s) => parseInt(s));
 
-  let seeds = [];
-  let i = 1;
-  for (let i = 1; i < seedsLine.length; i += 2) {
-    const baseNumber = seedsLine[i - 1];
-    const range = seedsLine[i];
-    for (let j = 0; j < range; j++) {
-      seeds.push(baseNumber + j);
-    }
-  }
-  console.log(seeds);
-
-  i = 1;
+  const maps = [];
+  let i = 3;
   while (i < data.length) {
     const map = [];
     while (data[i] !== "" && i < data.length) {
       map.push(data[i].split(" ").map((s) => parseInt(s)));
       i += 1;
     }
-    seeds = seeds.map((seed) => {
-      for (const mapEntry of map) {
-        const destinationStart = mapEntry[0];
-        const sourceStart = mapEntry[1];
-        const range = mapEntry[2];
-
-        if (seed >= sourceStart && seed < sourceStart + range) {
-          return seed + (destinationStart - sourceStart);
-        }
-      }
-      return seed;
-    });
+    maps.push(map);
     i += 2;
   }
-  console.log(seeds);
-  return Math.min(...seeds);
+
+  let minimum = Number.MAX_SAFE_INTEGER;
+  for (let idx = 1; idx < seedsLine.length; idx += 2) {
+    const baseNumber = seedsLine[idx - 1];
+    const range = seedsLine[idx];
+    console.log("Starting with seed", baseNumber, "until", baseNumber + range);
+    for (let offset = 0; offset < range; offset++) {
+      let seed = baseNumber + offset;
+      const temp = seed;
+      for (let x = 0; x < maps.length; x++) {
+        const mapEntry = maps[x];
+        for (let y = 0; y < mapEntry.length; y++) {
+          const destinationStart = mapEntry[y][0];
+          const sourceStart = mapEntry[y][1];
+          const range = mapEntry[y][2];
+          if (seed >= sourceStart && seed < sourceStart + range) {
+            seed = seed + (destinationStart - sourceStart);
+            break;
+          }
+        }
+      }
+      minimum = Math.min(minimum, seed);
+    }
+    console.log(minimum);
+  }
+  return minimum;
 }
 
 console.log("Solution to problem 1:", main1());
